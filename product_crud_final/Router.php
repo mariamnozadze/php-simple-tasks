12bfd2180e78b4d949d68c46d06a1f98db3a54ff
mariamnozadze <mariamnozadze97@yahoo.com>
2022-09-26 19:05:23 +0400

    - Practice with sessions  - displayed how many times has been page visited

--- product_crud_final/Router.php
+++ product_crud_final/Router.php
@@ -18,5 +18,21 @@ class Router
 
     public function resolve()
     {
+        $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
+        $method = strtolower($_SERVER['REQUEST_METHOD']);
+
+        if ($method === 'get') {
+            $fn = $this->getRoutes[$currentUrl] ?? null;
+        } else {
+            $fn = $this->postRoutes[$currentUrl] ?? null;
+        }
+
+        if ($fn) {
+            echo '<pre>';
+            var_dump($fn);
+            echo '</pre>';
+        } else {
+            echo 'page not found';
+        }
     }
 }
